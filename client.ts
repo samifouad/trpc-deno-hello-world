42304fc02aff3f07415f749ce0b2df9ef6a9438e
Sami Fouad <sfouad@gmail.com>
2022-08-30 03:34:26 -0600

    Create client.ts

---
+++ client.ts
@@ -0,0 +1,13 @@
+import type { AppRouter } from "./main.ts";
+import { createTRPCClient } from "https://esm.sh/@trpc/client@9.27.2";
+
+const client = createTRPCClient<AppRouter>({
+  url: 'http://localhost:5005/trpc',
+});
+
+try {
+  const query = await client.query("hw");
+  console.log(JSON.stringify(query));
+} catch (e) {
+  console.log(e);
+}
