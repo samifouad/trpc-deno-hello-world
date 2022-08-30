d2896412ca9391557f01fa57ccf80dabd2c9e0b4
Sami Fouad <sfouad@gmail.com>
2022-08-30 10:25:51 -0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,23 +1,19 @@
 import { opine } from "https://deno.land/x/opine@2.3.3/mod.ts";
 import * as trpc from "https://esm.sh/@trpc/server@9.27.2";
 import * as trpcExpress from "https://esm.sh/@trpc/server@9.27.2/adapters/express";
-import { opineCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+//import { opineCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
-const app = opine();
+const app = opine(); // opine is express ported to deno
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
+// setup tRPC router
 const appRouter = trpc.router().query("hw", {
   resolve() {
-    const data = {
-      hello: 'world'
-    }
+    const data = { hello: 'world' }
     return data;
   },
 });
 
+// apply tRPC router as a middleware
 app.use("/trpc",
   trpcExpress.createExpressMiddleware({
     router: appRouter,
@@ -25,11 +21,8 @@ app.use("/trpc",
   })
 );
 
-app.use(opineCors());
+//app.use(opineCors()); // uncomment to use cors
 
-app.listen(5005, () =>
-  console.log("server has started on http://localhost:5005")
-);
+app.listen(5005); // start server
 
-// trpc type export
-export type AppRouter = typeof appRouter;
+export type AppRouter = typeof appRouter; // tRPC type-only export
