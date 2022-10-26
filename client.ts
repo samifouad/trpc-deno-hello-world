d696d636154d527b99bb52dd214eb480d42bdc9b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-10-26 12:32:51 +0200

    update server.ts and client ts to trpc v10

--- client.ts
+++ client.ts
@@ -1,11 +1,22 @@
 import type { AppRouter } from "./server.ts";
-import { createTRPCClient } from "https://esm.sh/@trpc/client@9.27.2";
+import {
+  createTRPCProxyClient,
+  httpBatchLink,
+} from "https://esm.sh/@trpc/client@10.0.0-proxy-beta.26";
 
-const client = createTRPCClient<AppRouter>({ url: 'http://localhost:5005/trpc' });
+const client = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: "http://localhost:5005/trpc",
+    }),
+  ],
+});
 
 try {
-  const query = await client.query("hw");
-  console.log(JSON.stringify(query));
+  const query1 = await client.hello.query("World");
+  const query2 = await client.helloJson.query("World");
+  console.log(query1);
+  console.log(JSON.stringify(query2));
 } catch (e) {
   console.error(e);
 }
--- server.ts
+++ server.ts
@@ -1,25 +1,54 @@
 import { opine } from "https://deno.land/x/opine@2.3.3/mod.ts";
-import * as trpc from "https://esm.sh/@trpc/server@9.27.2";
-import * as trpcExpress from "https://esm.sh/@trpc/server@9.27.2/adapters/express";
+// TODO: Update imports to stable 10.0.0 when ready
+import {
+  inferAsyncReturnType,
+  initTRPC,
+} from "https://esm.sh/@trpc/server@10.0.0-proxy-beta.26";
+import * as trpcExpress from "https://esm.sh/@trpc/server@10.0.0-proxy-beta.26/adapters/express";
+// This zod import is for request validation
+import { z } from "https://deno.land/x/zod@v3.19.1/mod.ts";
 //import { opineCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+
+/**
+ * TRPC V10 Docs for Express integration:
+ * https://trpc.io/docs/v10/express#3-use-the-express-adapter
+ */
+
+// created for each request
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({}); // no context
+type Context = inferAsyncReturnType<typeof createContext>;
+
+const t = initTRPC.context<Context>().create();
+
+const appRouter = t.router({
+  hello: t.procedure
+    .input(z.string())
+    .query((req) => {
+      return `Hello ${req.input}`;
+    }),
+
+  helloJson: t.procedure
+    .input(z.string())
+    .query((req) => {
+      const data = { hello: req.input };
+      return data;
+    }),
+});
 
 const app = opine(); // opine is express ported to deno
 const port = 5005;
 
-// setup tRPC router
-const appRouter = trpc.router().query("hw", {
-  resolve() {
-    const data = { hello: "world" };
-    return data;
-  },
-});
-
 // apply tRPC router as a middleware
 app.use(
   "/trpc",
+  // @ts-expect-error Something is wrong with the typing here, prob. inside the createExpressMiddleware function,
+  // but it works when running `deno task server`
   trpcExpress.createExpressMiddleware({
     router: appRouter,
-    createContext: () => null,
+    createContext,
   }),
 );
 
